f448da04facbcb788370ca8f60b846604ae518b9
DonToli <strt87@yandex.ru>
2021-12-08 13:48:11 +0300

    22 lesson. Inkapsulaciy

---
+++ src/Lesson22/HomeWork1.java
@@ -0,0 +1,10 @@
+package Lesson22;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+    Student student1 = new Student(new StringBuilder("Fedor"), 3, 9);
+    student1.showInfo();
+
+}
+}
---
+++ src/Lesson22/Student.java
@@ -0,0 +1,45 @@
+package Lesson22;
+
+public  class Student {
+    private StringBuilder name;
+    private int course;
+    private int grade;
+
+    public Student(StringBuilder name, int course, int grade) {
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public StringBuilder getName() {
+        StringBuilder name2 = new StringBuilder(name);
+        return name2;
+    }
+
+    public void setName(StringBuilder name) {
+        if (name.length()>3){
+        this.name = name;}
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        if (course>=1 && course<5){
+        this.course = course;}
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        if (grade<0 && grade<11){
+        this.grade = grade;}
+    }
+
+    public  void showInfo(){
+        System.out.println("Name a student "+ getName()+", lerning in "+ getCourse()+" course, his grade "+getGrade());
+    }
+}
